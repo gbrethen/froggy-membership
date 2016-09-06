34ac63ab01b5b69ce9816a02e5fd794840a9350c
gbrethen <gbrethen@att.net>
2016-09-06 11:18:20 -0500

    Initial

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+/*jslint node:true */
+'use strict';
+var grunt = require('grunt');
+
+module.exports = function (grunt) {
+    
+    grunt.initConfig({
+        jshint : {
+            files : ['lib/**/*.js', 'models/**/*.js']
+        },
+        watch : {
+            files : ['lib/**/*.js', 'models/**/*.js'],
+            tasks : ['jshint']
+        }
+    });
+    
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+};
